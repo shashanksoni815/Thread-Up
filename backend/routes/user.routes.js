ccd33e8548142255758ca2723eb0ddfad0030c88
Shashank Soni <shashanksoni815@gmail.com>
2025-07-21 00:01:00 +0530

    push

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import {Router} from "express";
 // import { activeCheck } from "../controllers/post.controller.js";
-import { register, login, uploadProfilePicture, updateUserProfile, getUserAndProfile, updateProfileData } from "../controllers/user.controller.js";
+import { register, login, uploadProfilePicture, updateUserProfile, getUserAndProfile, updateProfileData, downloadProfile } from "../controllers/user.controller.js";
 import multer from "multer";
 
 const router = Router();
@@ -24,6 +24,7 @@ router.route('/login').post(login);
 router.route('/user_update').post(updateUserProfile)
 router.route('/get_user_and_profile').get(getUserAndProfile);
 router.route('/update_profile_data').get(updateProfileData);
-router.route('/user/download_resume')
+router.route('/user/download_resume').get(downloadProfile);
+router.route("/user/send_connection_request")
 
 export default router;
