164852c15f34a828c301ebbbaff32b9871381901
Shashank Soni <shashanksoni815@gmail.com>
2025-09-12 14:56:04 +0530

    database

---
+++ backendMain/models/connection.model.js
@@ -0,0 +1,7 @@
+// import mongoose from "mongoose";
+
+// const connectionRequest = new mongoose.Schema({
+//     userId: {
+//         type: mongoose
+//     }
+// })
--- backendMain/server.js
+++ backendMain/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import postRoute from "./routes/posts.routes.js"
 
 dotenv.config({
     path: "../.env"
@@ -10,7 +11,7 @@ dotenv.config({
 const app = express();
 
 app.use(cors());
-
+app.use(postRoute);
 app.use(express.json());
 
 const start = async () => {
