115fa4a886531c9e979a7a1deb2a6910d0d1f011
Shashank Soni <shashanksoni815@gmail.com>
2025-09-17 19:20:43 +0530

    hashed

--- backendMain/routes/user.routes.js
+++ backendMain/routes/user.routes.js
@@ -1,7 +1,19 @@
 import { Router } from "express";
 import { login, register } from "../controllers/user.controller.js";
-
+import multer from "multer";
 const router = Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({});
+
 
 router.route('/register').post(register);
 router.route('/login').post(login);
